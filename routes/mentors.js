93773f4b0ce31e77d407969620c9b4ee80acb2c7
nerdyOvel <elenwo.ovunda3@gmail.com>
2022-04-17 23:30:36 +0100

    created respective routes

---
+++ routes/mentors.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+
+//LOGIN ENDPOINT
+
+//REGISTER ENDPOINT
+
+//GRADE SUBMISSIONS
+
+//VIEW SUBMISSIONS
+
+//SELECT A SUBMISSION
+module.exports = router;
